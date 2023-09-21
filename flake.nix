bc1fc24f7b3387d050c9b27f86fecf13e9af5494
Brennan Seymour <brenno263@gmail.com>
2023-09-21 15:24:14 -0500

    basic, functioning flake setup

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Brennan Seymour's Nixos flake configuration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixos-hardware.url = "github:NixOS/nixos-hardware";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
+    };
+  };
+
+  outputs = { self, nixpkgs, nixpkgs-unstable, nixos-hardware, home-manager, ...}@inputs: {
+    nixosConfigurations = {
+      lucy = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+	specialArgs = inputs;
+        modules = [
+          nixos-hardware.nixosModules.system76
+          ./configuration.nix
+	  ./hardware-configuration.nix
+          home-manager.nixosModules.home-manager {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+          }
+        ];
+      };
+    };
+  };
+}
