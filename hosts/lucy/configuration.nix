42349398a6c605bc132c294476f8c593b150799d
Brennan Seymour <brenno263@gmail.com>
2026-01-04 23:10:57 -0600

    integrate lucy config

--- hosts/lucy/configuration.nix
+++ hosts/lucy/configuration.nix
@@ -9,17 +9,13 @@
     [
       flake-inputs.home-manager.nixosModules.default
       ./hardware-configuration.nix
-      ./nix-settings.nix
 
       # USERS (make sure there's at least one!!)
       ../../users/b
 
       # CUSTOM MODULES
-      # ../../modules/nvidia
-      ../../modules/amdgpu.nix
       ../../modules/gnome
-      ../../modules/gaming
-      # ../../modules/godot-3-libxcrypt.nix
+      ../../modules/nix-settings.nix
     ];
 
   # Users config
@@ -44,26 +40,7 @@
   # set kernel version
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
-
-  # set kernel module params
-  # boot.extraModprobeConfig = ''
-    # options usbhid mousepoll=8 jspoll=8 quirks=0x045e:0x028e:0x0400
-  # '';
-
-  # get wireshark workin
-  programs.wireshark.enable = true;
-  services.udev.extraRules = ''
-    SUBSYSTEM=="usbmon", GROUP="wireshark", MODE="0640"
-  ''; 
-
-
-
-  networking.hostName = "hypergamma"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  networking.hostName = "lucy"; # Define your hostname.
 
   # Enable networking
   networking.networkmanager.enable = true;
@@ -123,12 +100,6 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
 
   # Install firefox.
@@ -137,19 +108,9 @@
   # Allow unfree packages
   nixpkgs.config = {
     allowUnfree = true;
-
-    # setting cuda support allows us to build all kind of apps
-    # with gpu acceleration. Unfortunately they're not cached,
-    # so this induces a LOT of compilation :/
-    # I'm leaving it off unless I decide I really need it.
-    # cudaSupport = true;
   };
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
     vim
     wget
     curl
@@ -162,5 +123,4 @@
   ];
 
   system.stateVersion = "23.11"; # Did you read the comment?
-
 }
